49b66bcaba90a3161f762ae3b996b0a5fdd79ba8
Clark Jeria <clark.ajf@gmail.com>
2016-10-13 15:48:13 +0000

    passenger-tests.js edited online with Bitbucket

--- tests/passenger-tests.js
+++ tests/passenger-tests.js
@@ -8,7 +8,7 @@ passengerOne = {
   firstName: "John",
   lastName: "Smith",
   emailAddress: "test-9876345@example.com",
-  password: "anypwd",
+  password: "anypwd1234",
   addressLine1: "454 Main Street",
   addressLine2: "",
   city: "Anytown",
